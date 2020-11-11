a4781412ad38da39d8f6dcfe7f7b8e2d09d185ee
jscheiber22 <jscheiber22@gmail.com>
2020-11-10 19:04:47 -0800

    first attempt at setup script, should work but firefox might not oops not oops but should add honey gain and wine 8) no that owuld be hard :/

---
+++ general-setup.sh
@@ -0,0 +1,47 @@
+# setting everything up so now im making a script to autoamtei it but itll sucj tbh
+
+# BE SURE TO RUN AS NOT ROOT SO DONT USE SUDO ./SETUP.SH >:(
+
+echo ""
+echo "DO NOT RUN WITH SUDO, IT IS ALREADY IN THE CODE, IT WILL BREAK"
+echo ""
+
+sleep 5s
+
+echo ""
+echo "SETTING UP FUN DIRECTORIES :)"
+echo ""
+
+mkdir /home/%u/Programming
+
+echo ""
+echo "UPDATING/UPGRADING SYSTEM"
+echo ""
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
+
+
+echo ""
+echo "INSTALLING SYSTEM APPLICATIONS"
+echo ""
+
+sudo apt-get -y install git
+sudo apt-get -y upgrade python3
+sudo apt-get -y install python3-pip
+sudo apt-get -y install python3-tk
+
+echo ""
+echo "INSTALLING PIP PACKAGES"
+echo ""
+
+sudo python3 -m pip install selenium
+sudo python3 -m pip install webdriver_manager
+sudo python3 -m pip install bs4
+
+
+echo ""
+echo "Now sign in to firefox >:("
+echo ""
+
+firefox https://accounts.firefox.com/signin?context=fx_desktop_v3&entrypoint=fxa_app_menu&action=email&service=sync %u
